ee037b0af10502fde4c9fba433dd64968e2bd1b0
Marcos Marins <marcos-henrique2000@hotmail.com>
2021-02-16 12:41:01 -0300

    Adicionando migrations

--- SmartSchool.API/Models/Aluno.cs
+++ SmartSchool.API/Models/Aluno.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SmartSchool.API.Controllers
+namespace SmartSchool.API.Models
 {
     public class Aluno
     {
